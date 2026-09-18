dd4d1a0f0a180811cec8a7cc88ceeeced8f48334
1062738065 <stermoon2026@gmail.com>
2026-09-18 19:11:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/* إعدادات الاتصال بقاعدة البيانات (Google Sheets عبر Apps Script)
+   الصقي هنا رابط الـ Web app بعد النشر (ينتهي بـ /exec) */
+const CONFIG = {
+  API_URL: "PASTE_YOUR_WEB_APP_URL_HERE",
+};
