f80f21e497131122bd06811db22ee33b9eb5ef6a
1062738065 <stermoon2026@gmail.com>
2026-09-18 22:54:36 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* إعدادات الاتصال بقاعدة البيانات (Google Sheets عبر Apps Script)
-   الصقي هنا رابط الـ Web app بعد النشر (ينتهي بـ /exec) */
-const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxtP1ssSEcrTYcOLaxma0iKFx3YffaDOb5QE7yK9R3z1wZAVbj0DazGOZdu4F3QWZVijA/exec",
-};
