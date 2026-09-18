54caca0da63623636fae90abc9ac2b33243db743
1062738065 <stermoon2026@gmail.com>
2026-09-18 20:07:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* إعدادات الاتصال بقاعدة البيانات (Google Sheets عبر Apps Script)
    الصقي هنا رابط الـ Web app بعد النشر (ينتهي بـ /exec) */
-const CONFIG = {
+const CONFIG = {https://script.google.com/macros/s/AKfycbzMf5VNT9wziqY07fQQhuW2Id2PI6xptzXtRAtDJKyQPyjPmAR9HyJKNAMImUC8PTOhXA/exec
   API_URL: "PASTE_YOUR_WEB_APP_URL_HERE",
 };
